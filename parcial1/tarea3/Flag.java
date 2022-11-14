01bf991dbfe1d742ba8f9a32ab456b47ff487c19
claudiavasquez1 <117607275+claudiavasquez1@users.noreply.github.com>
2022-11-13 21:55:25 -0600

    Create Flag.java

---
+++ parcial1/tarea3/Flag.java
@@ -0,0 +1,14 @@
+/*
+*Flag.java
+*Author: claudiav
+*Location: Ixtac, Ver. Mexico
+*/
+
+class Flag { // name of Program
+  public static void main(String[] args) { // main
+
+    boolean flag = false;        // define a boolean var
+    System.out.println("Valor de flag: ");
+    System.out.println(!!flag);   //prints true
+    }
+}
