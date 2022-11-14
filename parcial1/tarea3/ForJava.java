707e4bc0b22745e5ca46e501b9274d0de899dfbb
claudiavasquez1 <117607275+claudiavasquez1@users.noreply.github.com>
2022-11-14 13:19:30 -0600

    Create ForJava.java

---
+++ parcial1/tarea3/ForJava.java
@@ -0,0 +1,12 @@
+// El programa imprime 5 lineas de texto
+
+class ForJava {
+    public static void main(String[] args) {
+  
+      int n = 1;
+      // for loop  
+      for (int i = 1; i <= n; ++i) {
+        System.out.println("Java is funny");
+      }
+    }
+  }
